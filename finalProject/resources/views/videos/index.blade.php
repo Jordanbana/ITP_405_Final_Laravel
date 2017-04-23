34e98af4a9a8a3908db510cacc936ef36b3684a0
Jordanbana <jordanbana@gmail.com>
2017-04-23 14:22:44 -0700

    front

--- finalProject/resources/views/videos/index.blade.php
+++ finalProject/resources/views/videos/index.blade.php
@@ -1,7 +1,21 @@
 <html>
 
-  @foreach ($videos as $video)
-    {{ $video->title }} 
-  @endforeach
+  <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <table>
+      <tbody>
+        @foreach ($videos as $video)
+          <tr>
+            <td>{{ $video->title }}</td>
+          </tr>
+          @endforeach
+      </tbody>
+    </table>
+
+  </body>
 
 </html>
